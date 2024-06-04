55a6346193883a078abd3eac851db9b971c8f61b
lilalr <gurennut@gmail.com>
2024-06-04 18:04:48 +0700

    perintah try

---
+++ manualException/manualException.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    try
+    {
+        cout << "Selamat belajar di Prodi TI UMY" << endl;
+        throw 0.5; // melemparkan sebuah integer maka
+        cout << "Pernyataan tidak akan dieksekusi" << endl;
+    }
+
+    catch (int a)
+    {
+        // blok ini akan dieksekusi
+        cout << "Pengecualian akan dieksekusi" << endl;
+    }
+
+    catch (...)
+    {
+        /*jika selain integer maka block ini akan dieksekusi*/
+        cout << "Default pengecualian dieksekusi" << endl;
+    }
+
+    return 0;
+}
